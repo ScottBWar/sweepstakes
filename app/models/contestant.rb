9699ab376d29dfedc7eed917cda5561fce6f7282
Scott Warner <scottbwarner@gmail.com>
2015-11-16 14:41:31 -0500

    validate email for uniqueness

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -4,16 +4,10 @@ class Contestant < ActiveRecord::Base
 
 	validates :lastname, :presence => true, :length => {:maximum => 30}
 
-	validates :email, :presence => true
+	validates :email, :birthday, :zipcode, :provider,  :presence => true
 
 	validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
 
-	validates :birthday, :presence => true
-
-	validates :zipcode, :presence => true
-
-	validates :provider, :presence => true
-
-
+  validates :email, :uniqueness => true
 
 end
