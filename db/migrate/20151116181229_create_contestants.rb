c7235bc003153ede6da09406a19851dab4e64dfb
Scott Warner <scottbwarner@gmail.com>
2015-11-16 13:48:36 -0500

    migrations, seed data, install faker

--- db/migrate/20151116181229_create_contestants.rb
+++ db/migrate/20151116181229_create_contestants.rb
@@ -1,6 +1,12 @@
 class CreateContestants < ActiveRecord::Migration
   def change
     create_table :contestants do |t|
+    	t.string :firstname, null: false
+    	t.string :lastname, null: false
+    	t.string :email, null: false
+    	t.string :birthday, null: false
+    	t.string :zipcode, null: false
+    	t.string :provider, null: false
 
       t.timestamps null: false
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+12.times do |t|
+Contestant.create({firstname: Faker::Name.first_name, lastname:Faker::Name.last_name, email:Faker::Internet.email, birthday:Faker::Time.backward(4000), zipcode:Faker::Address.zip, provider:Faker::Company.name})
+end
