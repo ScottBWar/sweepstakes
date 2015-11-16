8147f76cbe65c3b69f355b8345c96f9ac78ed9da
Scott Warner <scottbwarner@gmail.com>
2015-11-16 14:24:16 -0500

    add a directory in app for custom validators

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,2 +1,19 @@
 class Contestant < ActiveRecord::Base
+
+	validates :firstname, :presence => true, :length => {:maximum => 30}
+
+	validates :lastname, :presence => true, :length => {:maximum => 30}
+
+	validates :email, :presence => true
+
+	validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
+
+	validates :birthday, :presence => true
+
+	validates :zipcode, :presence => true
+
+	validates :provider, :presence => true
+
+
+
 end
